2f1c88647f854feaf2f02700aba52983c1d3a67a
Colton <coltonTB@me.com>
2018-02-08 21:00:06 -0500

    refactor

--- package.require.js
+++ package.require.js
@@ -7,6 +7,12 @@ define( [ 'module'
 	, './shaders/FilmShader.js',
 	, './shaders/RGBShiftShader.js',
 	, './shaders/StaticShader.js',
+	, './shaders/CopyShader.js',
+	, './postprocessing/EffectComposer.js',
+	, './postprocessing/RenderPass.js',
+	, './postprocessing/ShaderPass.js',
+	, './postprocessing/MaskPass.js',
+	, './libs/dat.gui.min.js',
 	], function(module){
 	THREEx.BadTVSound.baseUrl	= module.uri+'/../'
 });
